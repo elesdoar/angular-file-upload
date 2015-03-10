3e5af95f92730c01e8dc6a667c10329baf8b5ca0
Michael Salgado <michaelsalgado@gkudos.com>
2015-03-10 17:55:23 -0500

    Fix AngularAMD error

--- src/intro.js
+++ src/intro.js
@@ -1,14 +1,8 @@
 (function(angular, factory) {
     if (typeof define === 'function' && define.amd) {
-      if(angular) {
-        define('angular-file-upload', ['angular'], function(angular) {
-          return factory(angular);
-        });
-      } else {
-        define('angular-file-upload', ['angularAMD'], function() {
-          return factory(angular);
-        });
-      }
+      define('angular-file-upload', ['angularAMD'], function() {
+        return factory(angular);
+      });        
     } else {
         return factory(angular);
     }
